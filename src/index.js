ee5d54e74e7efccbd9084ecab87a350b8a65bb43
jpr6at <jpr6a@outlook.com>
2021-08-23 22:32:38 -0500

    Crear paquete de dependencia

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Jorge",
+    "Alfonso",
+    "Andrea",
+    "Jessica",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)]
+    console.log(message);
+}
+
+module.exports = { randomMsg };
